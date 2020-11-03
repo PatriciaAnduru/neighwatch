ffffdd69eecf3dd5f5d06d716cdcaf13866301ae
PatriciaAnduru <andurupatricia@gmail.com>
2020-11-03 12:30:14 +0300

    test

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import *
+
+
+
+class ProfileTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        
+        self.new_profile = Profile(user_id=2,hood_id=3,bio="just testing", email='patriciaanduru@gmail.com',name="Patricia",profile_pic="default.jpeg")
+
